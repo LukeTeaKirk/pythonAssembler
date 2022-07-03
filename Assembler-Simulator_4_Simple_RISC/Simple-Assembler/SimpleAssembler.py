793a01a324a8d8a64ceb3d0f2e58dbe8aef7c921
sudokick <thedigitalmanan@gmail.com>
2022-07-03 18:22:06 +0530

    added more handling

--- Assembler-Simulator_4_Simple_RISC/Simple-Assembler/SimpleAssembler.py
+++ Assembler-Simulator_4_Simple_RISC/Simple-Assembler/SimpleAssembler.py
@@ -1,4 +1,6 @@
-pc = 0
+import sys
+
+programCounter = 0
 notvar = 0
 variableStack = {}
 varCounter = 0 
@@ -8,36 +10,39 @@ flagsStack = [0,0,0,0] #EGLV,0123
 binaryStack = []
 errorStack = []
 program = []
-
+isa = ['var', 'add', 'sub', 'mov', 'ld', 'st', 'mul', 'div', 'rs', 'ls', 'xor', 'or', 'and', 'not', 'cmp', 'jmp', 'jlt', 'jgt', 'je', 'hlt']
 def main():
-    global program, pc, notvar, variableStack, varCounter, labelStack, registerStack, flagsStack, binaryStack, errorStack, program
+    global program, programCounter, notvar, variableStack, varCounter, labelStack, registerStack, flagsStack, binaryStack, errorStack, program
     program = takeInput()
     for x in program:
         if(len(x) == 0):
-            pc = pc + 1
+            programCounter = programCounter + 1
         else:
             commands = x.split(' ')
-            if(len(commands) > 4){
-                errorStack.append("Unexpected parameters in line number " + str(pc) + "\n" + "Line: " + x)
-            }
+            if(len(commands) > 5):
+                errorStack.append("Unexpected parameters in line number " + programCounter.__str__() + "\n" + "Line: " + x)
+            
             try:
+                if(commands[0][-1] == ':'):
+                    label(commands[0][:-1])
+                    commands = commands[1:]
                 if(commands[0] != "var"):
                     notvar = 1
                 if(commands[0] == 'var'):
                     if(len(commands) == 2):
                         var(commands[1])
                     else:
-                        errorStack.append("Unexpected parameters in line number " + str(pc) + "\n" + "Line: " + x)
+                        errorStack.append("Unexpected parameters in line number " + programCounter.__str__() + "\n" + "Line: " + x)
                 if(commands[0] == 'add'):
                     if(len(commands) == 4):
                         add(commands[1], commands[2], commands[3])
                     else:
-                        errorStack.append("Unexpected parameters in line number " + str(pc) + "\n" + "Line: " + x)
+                        errorStack.append("Unexpected parameters in line number " + programCounter.__str__() + "\n" + "Line: " + x)
                 if(commands[0] == 'sub'):
                     if(len(commands) == 4):
                         sub(commands[1], commands[2], commands[3])
                     else:
-                        errorStack.append("Unexpected parameters in line number " + str(pc) + "\n" + "Line: " + x)
+                        errorStack.append("Unexpected parameters in line number " + programCounter.__str__() + "\n" + "Line: " + x)
                 if(commands[0] == 'mov'):
                     if(len(commands) == 3):
                         if('$' in commands[2]):
@@ -48,7 +53,7 @@ def main():
                             else:
                                 movR(commands[1], commands[2])
                     else:
-                        errorStack.append("Unexpected parameters in line number " + str(pc) + "\n" + "Line: " + x)
+                        errorStack.append("Unexpected parameters in line number " + programCounter.__str__() + "\n" + "Line: " + x)
                 if(commands[0] == 'ld'):
                     load(commands[1], commands[2])
                 if(commands[0] == 'st'):
@@ -80,25 +85,29 @@ def main():
                 if(commands[0] == 'je'):
                     equalJmp(commands[1])
                 if(commands[0] == 'hlt'):
-                    hlt()
-                else:
-                    if(commands[0][-1] == ':'):
-                        label(commands[0][::-1])
-            except:
-                errorStack.append("Syntax error in line number " + str(pc)+ "\n" + "Line: " + x)
+                    hlt()   
+                if(commands[0] not in isa):
+                    errorStack.append("Unrecognized commands in line number: " + programCounter.__str__()+ "\n" + "Line: " + x)    
+            except IndexError:
+                errorStack.append("Expected more parameters in line number:  " + programCounter.__str__()+ "\n" + "Line: " + x)
+            except Exception as e:
+                errorStack.append("Syntax error in line number " + programCounter.__str__()+ "\n" + "Line: " + x)
             
-            pc = pc + 1
+            programCounter = programCounter + 1
     if(len(errorStack)==0):
-        for x in binaryStack:
+        for x in binaryStack[:256]:
             print(x)
     else:
         for x in errorStack:
             print(x)
+            print("\n")
     
 
 
 def takeInput():
-    listy = ['var X a','mov R1 $10', 'st R3 X','hlt']
+    listy = sys.stdin.read().split("\n")
+    listy = list(filter(None, listy))
+    print(listy)
     '''filey = open("input", "r")
     data = filey.read()
     listy = data.split("\n")'''
@@ -107,18 +116,18 @@ def takeInput():
 def checkRegBounds(r1, r2, r3):
     flag = 0
     if(not(r1[0] == 'R' and r2[0] == 'R' and r3[0] == 'R')):
-        errorStack.append("Syntax error, register not input correctly, Line Number: " + str(pc))
+        errorStack.append("Syntax error, register not input correctly, Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
     r1 = int(r1[1:])
     r2 = int(r2[1:])
     r3 = int(r3[1:])
     if(r1 > 6 or r1 < 0):
-        errorStack.append("Register is undefined R" + r1 + " Line Number: " + str(pc))
+        errorStack.append("Register is undefined R" + r1 + " Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
         flag = 1
     if(r2 > 6 or r2 < 0):
-        errorStack.append("Register is undefined R" + r2 + " Line Number: " + str(pc))
+        errorStack.append("Register is undefined R" + r2 + " Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
         flag = 1
     if(r3 > 6 or r3 < 0):
-        errorStack.append("Register is undefined R" + r3 + " Line Number: " + str(pc))
+        errorStack.append("Register is undefined R" + r3 + " Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
         flag = 1
     return flag
 
@@ -127,23 +136,23 @@ def checkWholeRange(val):
     flag = 0
     if(val>255 or val < 0):
         flag = 1
-        errorStack.append("lmm is out of range " + val + " Line Number: " + str(pc))
+        errorStack.append("lmm is out of range " + val + " Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
     if(val - int(val) != 0):
-        errorStack.append("lmm is not a whole number " + val + " Line Number: " + str(pc))
+        errorStack.append("lmm is not a whole number " + val + " Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
         flag = 1
     return flag
 
-def checkVar(vari):
-    if(vari not in variableStack):
+def checkVar(variz):
+    if(variz not in variableStack):
         if(label in labelStack):
-            errorStack.append("label is misused as a variable " + vari + " Line Number: " + str(pc))
-        errorStack.append("variable is undefined " + vari + " Line Number: " + str(pc))
+            errorStack.append("label is misused as a variable " + variz + " Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
+        errorStack.append("variable is undefined " + variz + " Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
 
 def checkLabel(addr):
     if(addr not in labelStack):
         if(addr in variableStack):
-            errorStack.append("variable is misused as a label " + addr + " Line Number: " + str(pc))
-        errorStack.append("label is undefined" + addr + " Line Number: " + str(pc))
+            errorStack.append("variable is misused as a label " + addr + " Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
+        errorStack.append("label is undefined" + addr + " Line Number: " + programCounter.__str__())
 
 def immtoBinary(val):
     try:
@@ -171,22 +180,23 @@ def regtoBinary(r1):
 def var(name):
     global varCounter, variableStack
     if(notvar == 1):
-        errorStack.append("Variables not declared at the beginning, Line: " + pc)
+        errorStack.append("Variables not declared at the beginning, Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
         return
     if(name in variableStack):
-        errorStack.append("Variables already declared")
+        errorStack.append("Variables already declared, Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
         return
     if(name in labelStack):
-        errorStack.append("Variable name already declared as label")
+        errorStack.append("Variable name already declared as label, Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
         return
     varCounter = varCounter + 1
     variableStack[name] = varCounter
 
 def label(name):
+    global varCounter, labelStack
     if(name in labelStack):
-        errorStack.append("Label already declared")
+        errorStack.append("Label already declared: " + name + " Line Number: " + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
         return
-    labelStack[name] = [pc, varCounter]
+    labelStack[name] = [programCounter, varCounter]
     varCounter = varCounter + 1
 
 
@@ -275,8 +285,8 @@ def equalJmp(addr):
         binaryStack.append('01111' + '000' + immtoBinary(labelStack.get(label)[1]))
 
 def hlt():
-    if(pc != len(program) - 1):
-        errorStack.append('halt command not the last command, line : ' + str(pc))
+    if(programCounter != len(program) - 1):
+        errorStack.append('halt command not the last command, line Number: ' + programCounter.__str__() + "\n" + "Line: " + program[programCounter])
     else:
         binaryStack.append('01010' + '00000000000')
 
